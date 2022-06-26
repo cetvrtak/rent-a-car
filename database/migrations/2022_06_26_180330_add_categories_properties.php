8eafa34d36b523d1a42d58024ae59ada1c4b1199
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2022-06-26 18:06:00 +0200

    Add categories' properties

---
+++ database/migrations/2022_06_26_180330_add_categories_properties.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->string('name');
+            $table->double('price');
+            $table->integer('parent_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropColumn(array('name', 'price', 'parent_id'));
+        });
+    }
+};
