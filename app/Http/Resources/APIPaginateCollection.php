915ec729122c569c81736ac514c9341b104db4eb
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2022-06-29 17:50:37 +0200

    Create pagination

---
+++ app/Http/Resources/APIPaginateCollection.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class APIPaginateCollection extends ResourceCollection
+{
+    private $resourceClass;
+    public $resource;
+
+    public function __construct($resource, $resourceClass) {
+        parent::__construct($resource);
+        $this->resource = $resource;
+        $this->resourceClass = $resourceClass;
+    }
+
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->resourceClass::collection($this->collection),
+            'pagination' => [
+                'current_page' => $this->resource->currentPage(),
+                'last_page' => $this->resource->lastPage(),
+                'per_page' => $this->resource->perPage(),
+                'first' => $this->resource->firstItem(),
+                'last' => $this->resource->lastItem(),
+                'count' => $this->resource->count(),
+                'total' => $this->resource->total()
+            ]
+        ];
+    }
+}
